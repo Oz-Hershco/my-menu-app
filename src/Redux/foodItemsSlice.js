3916d7c658fbdf99825014dd6c0c532b2ca3a399
Ozzy <scier666@gmail.com>
2022-02-17 20:04:20 +0200

    started filling out menu updated menu items container alignment

--- src/Redux/foodItemsSlice.js
+++ src/Redux/foodItemsSlice.js
@@ -5,29 +5,77 @@ export const foodItemsSlice = createSlice({
   initialState: {
     value: [
       {
-        id: "1d0f147f-5e6e-4502-9e4a-13c93fb779ca",
-        img: "http://www.easyrecipes.co.il/wp-content/uploads/2014/10/%D7%A2%D7%95%D7%92%D7%AA-%D7%91%D7%99%D7%A1%D7%A7%D7%95%D7%95%D7%99%D7%98%D7%99%D7%9D.jpg",
-        title: "Nutella Cake",
-        description: "A heart shaped cake with a biscuit base, frosting and kinder toppings. Extremely Delicious!",
-        price: "₪250",
-        type: {
-          id: "4f13ac6e-99b7-496f-9015-f2b4dac0110e",
-          name: "Sweet",
-          selected: false
-        },
-      },
-      {
-        id: "56f1b5b2-9b61-4d1f-a857-c2678527f43b",
-        img: "https://www.matkonimil.co.il/wp-content/uploads/2020/04/92319132_1082373325455744_6446888355136299457_n-1024x1024.jpg",
-        title: "Jerusalm Bagel",
-        description: "5/10 units of Jerusalem style bagel. super crusty, full of sesame seeds and really delicious! A great addition to your Israeli breakfast with the whole family.",
-        price: "₪35/₪65",
+        id: "1",
+        img: "https://iamafoodblog.b-cdn.net/wp-content/uploads/2019/02/full-english-7355w-2.jpg",
+        title: "English Breakfast",
+        description: "Bacon, sausages, eggs, tomatoes, mushrooms, toast, and beans all on one plate.",
+        price: "₪65",
         type: {
           id: "dd730a41-0af7-4020-ac3a-992d5603f063",
           name: "Savory",
           selected: false
         },
       },
+      {
+        id: "2",
+        img: "https://iamafoodblog.b-cdn.net/wp-content/uploads/2020/04/small-batch-sourdough-1941w-scaled.jpg",
+        title: "Sourdough Bread",
+        description: "Sourdough is loose term that refers to bread that’s been made with a wild yeast starter rather than commercial yeast.",
+        price: "₪35",
+        type: {
+          id: "dd730a41-0af7-4020-ac3a-992d5603f063",
+          name: "Savory",
+          selected: false
+        },
+      },
+      {
+        id: "3",
+        img: "https://iamafoodblog.b-cdn.net/wp-content/uploads/2022/01/tiktok-green-goddess-salad-0176w-1024x683.webp",
+        title: "Green Salad",
+        description: "The most satisfying vegan green goddess salad.",
+        price: "₪35",
+        type: {
+          id: "dd730a41-0af7-4020-ac3a-992d5603f063",
+          name: "Savory",
+          selected: false
+        },
+      },
+      // {
+      //   id: "1",
+      //   img: "http://www.easyrecipes.co.il/wp-content/uploads/2014/10/%D7%A2%D7%95%D7%92%D7%AA-%D7%91%D7%99%D7%A1%D7%A7%D7%95%D7%95%D7%99%D7%98%D7%99%D7%9D.jpg",
+      //   title: "Nutella Cake",
+      //   description: "A heart shaped cake with a biscuit base, frosting and kinder toppings. Extremely Delicious!",
+      //   price: "₪250",
+      //   type: {
+      //     id: "4f13ac6e-99b7-496f-9015-f2b4dac0110e",
+      //     name: "Sweet",
+      //     selected: false
+      //   },
+      // },
+      // {
+      //   id: "2",
+      //   img: "https://iamafoodblog.b-cdn.net/wp-content/uploads/2019/02/full-english-7355w-2.jpg",
+      //   title: "English Breakfast",
+      //   description: "Bacon, sausages, eggs, tomatoes, mushrooms, toast, and beans all on one plate.",
+      //   price: "₪65",
+      //   type: {
+      //     id: "dd730a41-0af7-4020-ac3a-992d5603f063",
+      //     name: "Savory",
+      //     selected: false
+      //   },
+      // },
+      // {
+      //   id: "3",
+      //   img: "https://www.matkonimil.co.il/wp-content/uploads/2020/04/92319132_1082373325455744_6446888355136299457_n-1024x1024.jpg",
+      //   title: "Jerusalm Bagel",
+      //   description: "5/10 units of Jerusalem style bagel. super crusty, full of sesame seeds and really delicious! A great addition to your Israeli breakfast with the whole family.",
+      //   price: "₪35/₪65",
+      //   type: {
+      //     id: "dd730a41-0af7-4020-ac3a-992d5603f063",
+      //     name: "Savory",
+      //     selected: false
+      //   },
+      // },
     ],
   },
   reducers: {
